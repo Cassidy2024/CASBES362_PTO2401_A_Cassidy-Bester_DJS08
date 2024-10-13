d223cd0bd24739b0588585171146048afbddd180
Cassidy2024 <157302466+Cassidy2024@users.noreply.github.com>
2024-10-13 20:58:43 +0400

    Code solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,67 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+
+
+
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from './Vans/Home.jsx';
+import About from './About.jsx';
+import Vans from "./Vans/Vans.jsx";
+import VansDetails from "./Vans/VansDetails.jsx";
+import Layout from "./components/Layout.jsx";
+import Dashboard from "./Host/Dashboard.jsx";
+import Income from "./Host/Income.jsx";
+import Reviews from "./Host/Reviews.jsx";
+import HostVans from "./Host/HostVans.jsx";
+import HostVanDetail from "./Host/HostVanDetail.jsx";
+import HostVanInfo from "./Host/HostVanInfo.jsx";
+import HostVanPricing from "./Host/HostVanPricing.jsx";
+import HostVanPhotos from "./Host/HostVanPhotos.jsx";
+import HostLayout from "./Host/HostLayout.jsx";
+import NotFound from "./Vans/NotFound.jsx";
+import Login from "./Vans/Login.jsx";
+import AuthRequired from "./components/AuthRequired.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 
   return (
-    <h1>Start here</h1>
-  )
+    
+      <BrowserRouter>
+        
+        <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />}/>
+          <Route path="/vans/:id" element={<VansDetails />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+
+
+            <Route element={<AuthRequired />}>
+              <Route path="/host" element={<HostLayout />}>
+                <Route index element={<Dashboard />} />
+                <Route path="income" element={<Income />} />
+                <Route path="reviews" element={<Reviews />} />
+                <Route path="vans" element={<HostVans />} />
+                <Route path="vans/:id" element={<HostVanDetail />}>
+                  <Route index element={<HostVanInfo />} />
+                  <Route path="pricing" element={<HostVanPricing />} />
+                  <Route path="photos" element={<HostVanPhotos />} />
+                </Route>
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
+    )
+  
+  
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import App from './App.jsx'
 import './index.css'
+
+
+
+import "./server"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
